160d9b1ef1a0db1e5b99c2497f3888e099b0ebd4
Sam Thomsen <sam@thomsens.net>
2021-03-03 11:00:27 -0400

    Adding Thomsen Style

--- thomsen/build-clang-format.cmd
+++ thomsen/build-clang-format.cmd
@@ -1,3 +1,3 @@
 @echo off
 
-cmake -DLLVM_ENABLE_PROJECTS=clang -DLLVM_USE_CRT_DEBUG=MTd -DLLVM_USE_CRT_RELEASE=MT -G "Visual Studio 15 2017" -A x64 -Thost=x64 ..\llvm
+devenv LLVM.sln /build Release /project tools\clang\tools\clang-format\clang-format.vcxproj
